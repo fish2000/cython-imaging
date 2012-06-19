74e59826fa6ba169834ecce4748e4d8f96527144
Alexander Böhn <fish2000@gmail.com>
2012-06-19 05:45:11 -0400

    Fleshing out setup.py and friends.

--- setup.py
+++ setup.py
@@ -7,7 +7,13 @@ Created by FI$H 2000 on 2012-06-19.
 Copyright (c) 2012 Objects In Space And Time, LLC. All rights reserved.
 """
 
+__author__ = 'Alexander Bohn'
+__version__ = (0, 2, 0)
+
+
 from distutils.core import setup, Extension
+from setuptools import find_packages
+
 
 try:
     import numpy
@@ -15,6 +21,7 @@ except ImportError:
     print("NumPy failed to import!")
     import sys
     sys.exit(1)
+
 
 ext_modules =   [
                     Extension('CIL.ext.rastersystem', sources=['CIL/ext/rastersystem.cpp'],
@@ -39,9 +46,55 @@ ext_modules =   [
                         ),
                 ]
 
-setup(
 
-    name='pHash',
-    version= '0.1.0',
-    description='C++ imaging power with Pythonic aplomb.',
-    ext_modules=ext_modules)
+setup(
+    name='django-signalqueue',
+    version='%s.%s.%s' % __version__,
+    description='Truly asynchronous signal dispatch for Django!',
+
+    author=__author__,
+    author_email='fish2000@gmail.com',
+    maintainer=__author__,
+    maintainer_email='fish2000@gmail.com',
+
+    license='BSD',
+    url='http://github.com/fish2000/django-signalqueue/',
+    keywords=[
+        'imaging',
+        'CImg', 'C++',
+        'CBIR', 'HDF5',
+        'image analysis',
+        'image comparison',
+        'image processing',
+        'perceptual hash',
+        'Cython',
+        'Ctypes',
+    ],
+
+    namespace_packages=['CIL'],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'CIL': [
+            'ext/*.cpp',
+            'ext/*.h',
+            'ext/include/*'],
+    },
+    install_requires=[
+        'django-delegate>=0.2.2', 'tornado', 'tornadio2', 'redis',
+    ],
+    tests_require=[
+        'nose', 'rednose', 'django-nose',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities'
+    ]
+)
+
